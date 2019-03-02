da1ae7ab3af240c7ce52f5adcbe50bed0fbbfbc7
Frank-Pasqualini <FJP11@pitt.edu>
2019-03-01 20:36:56 -0500

    Created the structure that reads in the controls and level info

---
+++ TetrisEvolve.lua
@@ -0,0 +1,10 @@
+if gameinfo.getromname() == "Tetris" then
+        Filename = "Level.state"
+        ButtonNames = {
+                "X",
+                "Y",
+                "Down",
+                "Left",
+                "Right",
+        }
+end
